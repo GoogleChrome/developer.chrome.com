0f49b1471e68b9d4aa1387baad08e91cc9973d8d
Rob Dodson <robdodson@google.com>
2020-09-09 15:58:05 -0700

    Fix busted lockfile and linter errors.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,9 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'npm run start',
-      url: [
-        "http://localhost:8080/",
-      ]
+      url: ['http://localhost:8080/'],
     },
   },
 };
