2b36250554195c8ac3136b8fe1871f34bebd0668
Rob Dodson <robdodson@users.noreply.github.com>
2020-09-09 17:20:09 -0700

    Add husky pre-push linter hook (#103)
    * wip
    
    * Fixes #101. Add husky pre-push hook.
    
    * Config scss linter.
    
    * empty
    
    * Fix missing dep.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
   extends: 'stylelint-config-sass-guidelines',
+  rules: {
+    // Allow a max nesting depth of 2
+    'max-nesting-depth': 2,
+
+    // Allow selectors like input[type=button]
+    // but not input.foo or input#foo
+    'selector-no-qualifying-type': [
+      true,
+      {
+        ignore: ["attribute"],
+      }
+    ]
+  }
 };
