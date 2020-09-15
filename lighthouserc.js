b3520d94983b076aa51982a60dfba5f4a26293fd
Ewa <devnook@gmail.com>
2020-09-16 01:50:09 +0200

    Add lighthouse preset target (#104)
    * Add lightouse preset target
    
    * Change warning to error in lhci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,16 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run start',
-      url: ['http://localhost:8080/'],
+      staticDistDir: './dist',
+    },
+    assert: {
+      // @see https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/configuration.md#preset
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'categories:performance': ['error', {minScore: 0.9}],
+        // Canonical url will not match the domain while on localhost.
+        canonical: 'off',
+      },
     },
   },
 };
