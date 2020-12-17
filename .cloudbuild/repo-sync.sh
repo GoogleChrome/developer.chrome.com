ffeb474dc481ebd56f7d7cf977c3cc1a91e26fbc
Rob Dodson <robdodson@google.com>
2020-12-17 13:35:52 -0800

    Add test branch

--- .cloudbuild/repo-sync.sh
+++ .cloudbuild/repo-sync.sh
@@ -1,1 +1,10 @@
-echo "hello world"
+# Grab the latest commits from the public repo
+git remote add public https://github.com/GoogleChrome/developer.chrome.com.git
+git fetch public
+
+# Create a test branch and replace its contents with main from the public repo
+git checkout -b test-branch
+git reset --hard public/main
+
+# Push the test branch to the private repo
+git push origin test-branch
