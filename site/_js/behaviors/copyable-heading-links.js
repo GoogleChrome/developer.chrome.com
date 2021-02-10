3f230c4ea5a816669ead8bee562a909cc903e5f6
tomayac <steiner.thomas@gmail.com>
2021-02-10 09:25:31 +0100

    Make sure we only run once

--- site/_js/behaviors/copyable-heading-links.js
+++ site/_js/behaviors/copyable-heading-links.js
@@ -29,14 +29,20 @@ const copyLinkToClipboard = () => {
         if (ev.target.nodeName === 'A') {
           ev.preventDefault();
         }
+        // Only run once.
+        if (heading.dataset.toasted) {
+          return;
+        }
         try {
           navigator.clipboard.writeText(
             `${location.origin}${location.pathname}#${heading.id}`,
           );
           const temp = heading.innerHTML;
-          heading.innerHTML += '&nbsp;<small>(📋 Copied.)</small>';
+          heading.innerHTML += '&nbsp;<small>(📋 Copied)</small>';
+          heading.dataset.toasted = 'toasted';
           setTimeout(() => {
             heading.innerHTML = temp;
+            delete heading.dataset.toasted;
           }, 2_000);
         } catch (err) {
           console.warn(err.name, err.message);
