5ebd0b825673ab21288aede65fbd6dd5fa77e6ee
Sam Thorogood <samthor@users.noreply.github.com>
2021-03-17 09:33:01 +1100

    details fixes (#493)
    * cleanpus to details
    
    * linter
    
    * misc details fixes
    
    * linter

--- site/_shortcodes/DetailsSummary.js
+++ site/_shortcodes/DetailsSummary.js
@@ -4,7 +4,7 @@
  */
 function DetailsSummary(content) {
   // Whitespace is needed for the markdown parser to process the ${content}.
-  return `<summary class="details__summary"> <div class="details__header">
+  return `<summary class="details__summary"> <div class="details__header stack">
 
 ${content}</div></summary>`;
 }
