48b9b99459fcb29880064b325ea8c22933be886c
Sam Thorogood <sam.thorogood@gmail.com>
2021-04-15 15:28:02 +1000

    improvements to resolve

--- site/_filters/md.js
+++ site/_filters/md.js
@@ -3,22 +3,63 @@ const md = require('markdown-it')();
 const linkMatch = /{@link (\S+?)(|\s+\S+?)}/g;
 
 /**
+ * Resolves a reference to another type from within Chrome APIs extensions docs.
+ *
+ * Refs are sourced directly from code, and may appear as e.g., "events.Event" or "lastError".
+ *
  * @param {string} ref
  * @return {string|undefined}
  * @this {any}
  */
-function resolveRawRef(ref) {
-  // console.info('requested link for', ref, 'this API is', this.ctx.api);
-
-  // TODO: demo for links
-  if (ref === 'events.Event') {
-    if (this.ctx.api === 'events') {
-      return '#type-Event';
-    }
-    return '../events/#type-Event';
+function modelToHref(ref) {
+  const namespace = this.ctx.namespace;
+  if (namespace === undefined) {
+    // This occurs on pages like the top-level reference page which has no specific current API.
+    // We could link through to the sub-pages from the description notices.
+    return undefined;
   }
 
-  return undefined;
+  const parts = ref.split('.');
+  let property = namespace.all[parts[0]];
+  let otherNamespace;
+
+  if (!property) {
+    // If we can't resolve this locally, then try to match it against another namespace.
+    const {chromeApiNamespaces} = this.ctx;
+    if (!chromeApiNamespaces) {
+      return;
+    }
+    otherNamespace = chromeApiNamespaces[parts[0]];
+    if (!otherNamespace) {
+      return;
+    }
+    property = otherNamespace.all[parts[1]];
+    if (!property) {
+      return;
+    }
+  }
+
+  /** @type {string} */
+  let prefix;
+  if (property.type.key === 'function') {
+    prefix = 'method';
+  } else if (
+    property.type.key === 'ref' &&
+    property.type.name === 'events.Event'
+  ) {
+    prefix = 'event';
+  } else if (property.isType) {
+    prefix = 'type';
+  } else {
+    prefix = 'property';
+  }
+
+  let href = otherNamespace
+    ? `../${otherNamespace.name.replace(/\./g, '_')}/`
+    : '';
+  href += `#${prefix}-${property.name}`;
+
+  return href;
 }
 
 function updateAtLink(content, resolver) {
@@ -47,7 +88,7 @@ function render(content) {
     return;
   }
 
-  const resolver = resolveRawRef.bind(this);
+  const resolver = modelToHref.bind(this);
   content = updateAtLink(content, resolver);
   return md.render(content);
 }
@@ -64,24 +105,9 @@ function renderInline(content) {
     return;
   }
 
-  const resolver = resolveRawRef.bind(this);
+  const resolver = modelToHref.bind(this);
   content = updateAtLink(content, resolver);
   return md.renderInline(content);
-}
-
-/**
- * @param {{
- *   name: string,
- *   internal: boolean,
- * }} model
- * @return {string|undefined}
- * @this {any}
- */
-function modelToHref(model) {
-  if (!model.internal) {
-    return;
-  }
-  return resolveRawRef.call(this, model.name);
 }
 
 module.exports = {render, renderInline, modelToHref};
