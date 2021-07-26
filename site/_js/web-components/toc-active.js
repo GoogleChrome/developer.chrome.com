c76689e888c9402ec1a748cf19c1ca7805b7b43b
Sam Thorogood <sam.thorogood@gmail.com>
2021-07-26 14:34:38 +1000

    replace with toc-active which highlights best link

---
+++ site/_js/web-components/toc-active.js
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Selects the most relevant sub-element in a TOC. Sets
+ * `[toc--active]` on the contained <a href> that matches the heading most
+ * visible on the page.
+ *
+ * Operates on a single `<article>` found in the document. Refuses to operate
+ * if it's not found.
+ */
+import {BaseElement} from './base-element';
+
+export class TocActive extends BaseElement {
+  constructor() {
+    super();
+    this.updateHeading = () => {};
+
+    /** @type {HTMLAnchorElement?} */
+    this.previousActiveAnchor = null;
+
+    /** @type {Set<HTMLElement>} */
+    this.visibleHeadings = new Set();
+
+    let rafPending = 0;
+
+    const options = {threshold: 0.5};
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const t = /** @type {HTMLElement} */ (entry.target);
+        if (entry.isIntersecting) {
+          this.visibleHeadings.add(t);
+        } else {
+          this.visibleHeadings.delete(t);
+        }
+
+        if (rafPending) {
+          return;
+        }
+        rafPending = window.requestAnimationFrame(() => {
+          rafPending = 0;
+          this.updateHeading();
+        });
+      });
+    }, options);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    // Make sure we have an article. Abandon if not.
+    const se = document.scrollingElement;
+    const target = document.body.querySelector('article');
+    if (!target || !se) {
+      return;
+    }
+
+    /** @type {{[hash: string]: HTMLAnchorElement}} */
+    const tocLinkDict = {};
+    const links = /** @type {NodeListOf<HTMLAnchorElement>} */ (
+      this.querySelectorAll('a[href]')
+    );
+    for (const link of links) {
+      const rawHref = link.getAttribute('href') ?? '';
+      if (rawHref.startsWith('#')) {
+        tocLinkDict[rawHref.substr(1)] = link;
+      }
+    }
+
+    // Add all the headings in the article to an IntersectionObserver.
+    const allArticleHeadings = /** @type {NodeListOf<HTMLElement>} */ (
+      target.querySelectorAll('[id]')
+    );
+    for (const articleHeading of allArticleHeadings) {
+      this.observer.observe(articleHeading);
+    }
+
+    this.updateHeading = () => {
+      // Find the first visible anchor element inside the article.
+      /** @type {HTMLAnchorElement?} */
+      let found = null;
+      for (const articleHeading of allArticleHeadings) {
+        if (
+          this.visibleHeadings.has(articleHeading) &&
+          articleHeading.id in tocLinkDict
+        ) {
+          found = tocLinkDict[articleHeading.id];
+          break;
+        }
+      }
+
+      if (found === this.previousActiveAnchor) {
+        return;
+      }
+
+      if (this.previousActiveAnchor) {
+        this.previousActiveAnchor.removeAttribute('toc--active');
+      }
+      if (found) {
+        found.setAttribute('toc--active', '');
+      }
+      this.previousActiveAnchor = found;
+    };
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.observer.disconnect();
+  }
+}
+
+customElements.define('toc-active', TocActive);
