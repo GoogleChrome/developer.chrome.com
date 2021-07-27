cd7a5a2119d3dd09fa666410915ff1b63a4e920c
Rob Dodson <robdodson@google.com>
2021-07-27 14:38:57 -0700

    add a sitemap.xml

--- site/_data/lib/date.js
+++ site/_data/lib/date.js
@@ -1,4 +1,13 @@
-const formatDate = (date, locale = 'en', opts) => {
+/**
+ * Returns a formatted date string.
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+ * for a list of options.
+ * @param {Date} date
+ * @param {string} [locale="en"]
+ * @param {object} opts
+ * @returns {string}
+ */
+const formatDate = (date, locale = 'en', opts = {}) => {
   if (typeof date === 'string') {
     date = new Date(date);
   }
@@ -9,6 +18,13 @@ const formatDate = (date, locale = 'en', opts) => {
   return new Intl.DateTimeFormat(locale, opts).format(date);
 };
 
+/**
+ * Returns a date string formatted like "Monday, July 26, 2021".
+ * @param {Date} date
+ * @param {string} [locale="en"]
+ * @param {object} opts
+ * @returns {string}
+ */
 const formatDateLong = (date, locale = 'en', opts = {}) => {
   const options = {
     weekday: 'long',
@@ -22,6 +38,13 @@ const formatDateLong = (date, locale = 'en', opts = {}) => {
   return formatDate(date, locale, options);
 };
 
+/**
+ * Returns a date string formated like "July 26, 2021".
+ * @param {Date} date
+ * @param {string} [locale="en"]
+ * @param {object} opts
+ * @returns {string}
+ */
 const formatDateShort = (date, locale = 'en', opts = {}) => {
   const options = {
     month: 'long',
@@ -34,4 +57,17 @@ const formatDateShort = (date, locale = 'en', opts = {}) => {
   return formatDate(date, locale, options);
 };
 
-module.exports = {formatDate, formatDateLong, formatDateShort};
+/**
+ * Returns a date string formatted like "2021-12-31".
+ * @param {Date} date
+ */
+const formatDateNumeric = date => {
+  return date.toISOString().split('T')[0];
+};
+
+module.exports = {
+  formatDate,
+  formatDateLong,
+  formatDateShort,
+  formatDateNumeric,
+};
