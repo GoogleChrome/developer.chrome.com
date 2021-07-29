096667b7dd5e7e5cb5a2b53760cbde5a936c55fd
Michael Solati <mkslt04@gmail.com>
2021-07-28 22:11:32 -0700

    chore(dependencies): update infra dependency (#1056)

--- site/_shortcodes/Video.js
+++ site/_shortcodes/Video.js
@@ -7,7 +7,7 @@ const {Video: BuildVideoShortcode} = require('webdev-infra/shortcodes/Video');
 const {bucket, imgixDomain} = require('../_data/site.json');
 
 /**
- * @param {wd.VideoArgs} args Named arguments
+ * @param {import('webdev-infra/types').VideoArgs} args Named arguments
  * @returns {string}
  */
 const Video = BuildVideoShortcode(bucket, imgixDomain);
