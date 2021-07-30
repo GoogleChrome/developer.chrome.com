b5c66a746f5bd536dc4d07cd94cc118d28a641f8
Sam Thorogood <sam.thorogood@gmail.com>
2021-07-30 10:08:52 +1000

    Revert "Revert "makes desktop toc sticky + highlight top link (#1033)" (#1067)"
    This reverts commit 9006c0ede5d2ae7c3b878f29ba77c01344334253.

--- site/_js/main.js
+++ site/_js/main.js
@@ -25,4 +25,5 @@ import './web-components/top-nav';
 import './web-components/share-button';
 import './web-components/web-tabs';
 import './web-components/language-select';
+import './web-components/toc-active';
 import './third-party/announcement-banner/announcement-banner';
