7502a1f2783d57bceea88da125c426e3969b7267
Sam Thorogood <samthor@users.noreply.github.com>
2021-08-05 09:56:11 +1000

    boop webdev-infra for imgix auto format (#1095)
    * bump webdev-infra and add some comments
    
    * fix

--- site/_shortcodes/Img.js
+++ site/_shortcodes/Img.js
@@ -1,8 +1,19 @@
-/**
- * Temporarily disable TS type check as the global `wd` namespace
- * that comes from `webdev-infra` is currently broken.
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-// @ts-nocheck
+
 const {imgix: imgixFilter} = require('webdev-infra/filters/imgix');
 const {Img: BuildImgShortcode} = require('webdev-infra/shortcodes/Img');
 
