64c2940c97397783044d0622394a10e92f04c8a6
Sam Thorogood <sam.thorogood@gmail.com>
2021-08-20 14:39:49 +1000

    show all named

--- site/_utils/tags-11tydata.js
+++ site/_utils/tags-11tydata.js
@@ -25,120 +25,94 @@ const {i18n} = require('../_filters/i18n');
  * @param {string} locale
  * @return {EleventyData}
  */
-module.exports = locale => {
-  /** @type {(name: string) => any} */
-  const namedRelease = name => {
-    return data => {
+module.exports = locale => ({
+  eleventyComputed: {
+    /**
+     * Generates tags for the current named releases: Stable, Beta, and so on. Ordered by earliest
+     * release first (i.e., 'Stable' > 'Beta' > 'Dev' > 'Canary').
+     */
+    namedChromeReleaseTags: data => {
+      const rawChannels = data.chrome.channels;
+
       /** @type {Tags} */
       const rawTags = data.collections.tags;
 
-      const release = data.chrome.channels[name]?.mstone;
-      if (!release) {
-        return undefined;
-      }
+      const all = [];
 
-      const key = `chrome-${release}`;
-      if (!rawTags[key]?.posts[locale]?.length) {
-        return undefined;
-      }
-
-      const tag = rawTags[key];
-      return {
-        key,
-        posts: tag.posts[locale],
-        title: i18n('i18n.common.release_' + name, locale),
-        release,
-      };
-    };
-  };
-
-  return {
-    eleventyComputed: {
-      stableReleaseTag: namedRelease('stable'),
-      betaReleaseTag: namedRelease('beta'),
-
-      namedChromeReleaseTags: data => {
-        const rawChannels = data.chrome.channels;
-
-        /** @type {Tags} */
-        const rawTags = data.collections.tags;
-
-        const all = [];
-
-        for (const [name, data] of Object.entries(rawChannels)) {
-          // name = 'stable', 'beta' etc
-          const release = data.mstone;
-          if (typeof release !== 'number') {
-            throw new Error(`exepected release number, was: ${release}`);
-          }
-
-          const key = `chrome-${release}`;
-          const tag = rawTags[key];
-          if (!tag?.posts[locale].length) {
-            continue;
-          }
-
-          all.push({
-            key: `chrome-${release}`,
-            posts: tag.posts[locale],
-            title: i18n('i18n.common.release_' + name, locale),
-            release,
-          });
+      for (const [name, data] of Object.entries(rawChannels)) {
+        // name = 'stable', 'beta' etc
+        const release = data.mstone;
+        if (typeof release !== 'number') {
+          throw new Error(`exepected release number, was: ${release}`);
         }
 
-        // Sort highest release first.
-        all.sort(({release: a}, {release: b}) => b - a);
+        // If there's no posts for this named release, skip.
+        const key = `chrome-${release}`;
+        const tag = rawTags[key];
+        if (!tag?.posts[locale].length) {
+          continue;
+        }
 
-        return all;
-      },
+        all.push({
+          key: `chrome-${release}`,
+          posts: tag.posts[locale],
+          title: i18n('i18n.common.release_' + name, locale),
+          release,
+        });
+      }
 
-      chromeReleaseTags: data => {
-        /** @type {Tags} */
-        const rawTags = data.collections.tags;
+      // Sort lowest release first.
+      all.sort(({release: a}, {release: b}) => a - b);
 
-        // Create an array of tags releated to releases.
-        const all = Object.values(rawTags)
-          .filter(tag => tag.release)
-          .map(tag => {
-            const release = /** @type {number} */ (tag.release);
-            return {
-              key: tag.key,
-              posts: tag.posts[locale] ?? [],
-              title: tag.overrideTitle ?? '?',
-              release,
-            };
-          });
-
-        // Sort highest release first.
-        all.sort(({release: a}, {release: b}) => b - a);
-
-        // Only return if there's locale-valid posts.
-        return all.filter(tag => tag.posts.length);
-      },
-
-      displayTags: data => {
-        /** @type {Tags} */
-        const rawTags = data.collections.tags;
-
-        // Create an array of tags unrelated to releases.
-        const all = Object.values(rawTags)
-          .filter(tag => !tag.release)
-          .map(tag => {
-            return {
-              key: tag.key,
-              posts: tag.posts[locale] ?? [],
-              title: i18n(tag.title, locale),
-            };
-          });
-
-        // Sort lexiographically by name.
-        all.sort((a, b) =>
-          a.title.toLowerCase().localeCompare(b.title.toLowerCase())
-        );
-
-        // Only return if there's locale-valid posts.
-        return all.filter(tag => tag.posts.length);
-      },
+      return all;
     },
-  };
-};
+
+    chromeReleaseTags: data => {
+      /** @type {Tags} */
+      const rawTags = data.collections.tags;
+
+      // Create an array of tags releated to releases.
+      const all = Object.values(rawTags)
+        .filter(tag => tag.release)
+        .map(tag => {
+          const release = /** @type {number} */ (tag.release);
+          return {
+            key: tag.key,
+            posts: tag.posts[locale] ?? [],
+            title: `Chrome ${release}`,
+            release,
+          };
+        });
+
+      // Sort highest release first.
+      all.sort(({release: a}, {release: b}) => b - a);
+
+      // Only return if there's locale-valid posts.
+      return all.filter(tag => tag.posts.length);
+    },
+
+    displayTags: data => {
+      /** @type {Tags} */
+      const rawTags = data.collections.tags;
+
+      // Create an array of tags unrelated to releases.
+      const all = Object.values(rawTags)
+        .filter(tag => !tag.release)
+        .map(tag => {
+          return {
+            key: tag.key,
+            posts: tag.posts[locale] ?? [],
+            title: i18n(tag.title, locale),
+          };
+        });
+
+      // Sort lexiographically by name.
+      all.sort((a, b) =>
+        a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+      );
+
+      // Only return if there's locale-valid posts.
+      return all.filter(tag => tag.posts.length);
+    },
+  },
+});
