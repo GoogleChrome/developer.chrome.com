db7636ec8e4fa040066876a0d0d6e84d50b3364f
Michael Solati <mkslt04@gmail.com>
2022-01-05 16:30:57 -0800

    fix(workbox): only link docs and references that actually exist

--- site/en/docs/workbox/modules/modules.11tydata.js
+++ site/en/docs/workbox/modules/modules.11tydata.js
@@ -26,6 +26,9 @@ module.exports = {
   // This is the default hero image for all Workbox pages.
   hero: 'image/QMjXarRXcMarxQddwrEdPvHVM242/ZUBXF0hK0jo9q4RvELUs.png',
   eleventyComputed: {
-    reference: data => `/docs/workbox/reference/${data.title}/`,
+    reference: data =>
+      data.workboxApiModules[data.title]
+        ? `/docs/workbox/reference/${data.title}/`
+        : null,
   },
 };
