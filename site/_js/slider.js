2abbf81027966adb63819ac93670c97c0a96ff6d
nbhansali@google.com <nbhansali@google.com>
2022-04-04 16:40:22 +0530

    safari nav issue fix on page refresh

--- site/_js/slider.js
+++ site/_js/slider.js
@@ -10,7 +10,8 @@ const cssClasses = {
   MENU_ITEM_LINK: 'sectionLink',
   CHROME_LOGO: 'chrome-logo',
   YEAR_SECTION: 'inner-card',
-  HIDE_NAVIGATION: 'hide-navigation',
+  HIDE_ON_FOOTER: 'hide-on-footer',
+  HIDE_ON_PRE_FOOTER: 'hide-on-pre-footer',
   SECTION_2022: 'section2022',
   SECTION_2008: 'section2008',
   SCROLL_SECTION: 'scroll-section',
@@ -19,9 +20,11 @@ const cssClasses = {
   CARD_SECTION: 'card-section',
   FOOTER_TOP_SECTION: 'footer-top-section',
   CHROME_LOGO_TEXT: 'chrome-logo-wrapper--text',
+  PRE_FOOTER: 'footer-top-section',
+  FOOTER: 'type--footer',
 };
 const imgBaseUrl = 'https://wd.imgix.net/image/H2WDdWf5aPXOtVabf53xIxMJyTF2/';
-
+ 
 const chromeLogoVersions = new Map();
 chromeLogoVersions.set(['2008', '2009', '2010'], '6Zok8fOGKlKnPmmA3BNM.png');
 chromeLogoVersions.set(['2011', '2012', '2013'], 'AkT3tMmLmIlEphta9Zv0.png');
@@ -34,6 +37,12 @@ class Slider {
   constructor() {
     this.currentIndex = 0;
     this.previousIndex = 0;
+    this.navigationWrapper = document.querySelector(
+      `.${cssClasses.NAVIGATION_WRAPPER}`
+    );
+    this.lastYearSec = document.querySelector(`#${cssClasses.SECTION_2022}`);
+    this.lastYearSecTop =
+      this.lastYearSec.offsetTop + this.lastYearSec.offsetHeight;
     this.initialize();
   }
   initialize() {
@@ -41,6 +50,11 @@ class Slider {
       `.${cssClasses.SCROLL_SECTION}`
     );
     const scrollToTopSec = document.querySelector(`.${cssClasses.BACK_TO_TOP}`);
+    const footerElement = document.querySelector(`.${cssClasses.FOOTER}`);
+    const preFooterElement = document.querySelector(
+      `.${cssClasses.PRE_FOOTER}`
+    );
+ 
     window.addEventListener('load', () => {
       /* eslint-disable no-undef */
       const yearTimeline = gsap.timeline();
@@ -49,6 +63,7 @@ class Slider {
       yearTimeline.add(this.clickNavLink());
       yearTimeline.add(this.handleTab());
     });
+    // adding click event listener for scroll to begin.
     scrollToFristSec.addEventListener('click', e => {
       e.preventDefault();
       const firstSec = document.querySelector(`#${cssClasses.SECTION_2008}`);
@@ -58,6 +73,8 @@ class Slider {
         ease: 'power1.inOut',
       });
     });
+ 
+    // adding click event listener for back to top
     scrollToTopSec.addEventListener('click', e => {
       e.preventDefault();
       const firstSec = document.querySelector(`.${cssClasses.HERO_SECTION}`);
@@ -67,7 +84,55 @@ class Slider {
         ease: 'power1.inOut',
       });
     });
+ 
+    // Adding Observer on whndow to hide year navigation on prefooter/footer section
+    Observer.create({
+      target: window,
+      type: 'wheel',
+      onUp: () => {
+        this.hideNavigation(window.pageYOffset, this.lastYearSecTop);
+      },
+      onDown: () => {
+        this.hideNavigation(window.pageYOffset, this.lastYearSecTop);
+      },
+    });
+ 
+    // Register IntersectionObserver for pre footer section
+    const preFooterIntersection = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          this.navigationWrapper.classList.add(cssClasses.HIDE_ON_PRE_FOOTER);
+        } else {
+          this.navigationWrapper.classList.remove(
+            cssClasses.HIDE_ON_PRE_FOOTER
+          );
+        }
+      });
+    });
+ 
+    // Register IntersectionObserver for footer section
+    const footerIntersection = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          this.navigationWrapper.classList.add(cssClasses.HIDE_ON_FOOTER);
+        } else {
+          this.navigationWrapper.classList.remove(cssClasses.HIDE_ON_FOOTER);
+        }
+      });
+    });
+ 
+    preFooterIntersection.observe(preFooterElement);
+    footerIntersection.observe(footerElement);
   }
+ 
+  hideNavigation(windowScroll, targetOffSet) {
+    if (windowScroll >= targetOffSet) {
+      this.navigationWrapper.classList.add(cssClasses.HIDE_ON_FOOTER);
+    } else {
+      this.navigationWrapper.classList.remove(cssClasses.HIDE_ON_FOOTER);
+    }
+  }
+ 
   startup() {
     const yearNavigationTimeline = gsap.timeline({
       id: 'Loading year navigation',
@@ -87,21 +152,15 @@ class Slider {
   scrollSections() {
     let oldScrollValue = 0;
     let newScrollValue = 0;
-
+ 
     const firstYearSection = document.querySelector(
       `.${cssClasses.CARD_SECTION}`
     );
-    const navigationWrapper = document.querySelector(
-      `.${cssClasses.NAVIGATION_WRAPPER}`
-    );
     const chromeLogo = document.querySelector(`.${cssClasses.CHROME_LOGO}`);
-    const fixedMenu = document.querySelector(
-      `.${cssClasses.FIXED_NAVIGATION_WRAPPER}`
-    );
     const chromeLogoYear = document.querySelector(
       `.${cssClasses.CHROME_LOGO_TEXT}`
     );
-
+ 
     gsap.utils.toArray(`.${cssClasses.YEAR_SECTION}`).forEach(section => {
       const activeSection = section.id;
       const menuitem = 'menu__'.concat(activeSection);
@@ -119,16 +178,16 @@ class Slider {
             const isActive = self.isActive;
             const activeItemIndex = self.trigger.dataset.index;
             const sectionId = self.trigger.id.match(/\d+/g)[0];
-
+ 
             for (const [key, value] of chromeLogoVersions.entries()) {
               if (key.includes(sectionId)) {
                 chromeLogo.src = `${imgBaseUrl}${value}`;
                 chromeLogo.srcset = `${imgBaseUrl}${value}`;
               }
             }
-
+ 
             chromeLogoYear.textContent = sectionId;
-
+ 
             if (oldScrollValue < newScrollValue) {
               if (isActive) {
                 this.currentIndex = activeItemIndex;
@@ -146,34 +205,21 @@ class Slider {
           },
           start: 'top 20%',
           end: '50%',
-          onUpdate: self => {
+          onUpdate: () => {
             if (window.pageYOffset >= firstYearSection.offsetTop) {
-              navigationWrapper?.classList.add(
+              this.navigationWrapper?.classList.add(
                 cssClasses.FIXED_NAVIGATION_WRAPPER
               );
             } else {
-              navigationWrapper?.classList.remove(
+              this.navigationWrapper?.classList.remove(
                 cssClasses.FIXED_NAVIGATION_WRAPPER
               );
-            }
-
-            //Handling side nav when user scroll to the footer
-            if (fixedMenu !== undefined) {
-              if (self.trigger.id === 'section2022') {
-                if (window.pageYOffset >= self.end) {
-                  navigationWrapper?.classList.add(cssClasses.HIDE_NAVIGATION);
-                } else {
-                  navigationWrapper?.classList.remove(
-                    cssClasses.HIDE_NAVIGATION
-                  );
-                }
-              }
             }
           },
           toggleActions: 'play reverse play reverse',
         },
       });
-
+ 
       yearScrollTimeline.to(
         menulink,
         {
