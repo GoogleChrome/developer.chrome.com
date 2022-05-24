56fd4095eff3cd091943b32e7a7d8b75979f4afd
Ewa Gasperowicz <devnook@gmail.com>
2022-05-24 14:32:37 +0200

    lint

--- external/build/fugu-showcase.js
+++ external/build/fugu-showcase.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const {default: fetch} = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
-
-const url = 'https://sheets.googleapis.com/v4/spreadsheets/1S_Apr0HavFCO7H9hKcRjIUrgoT7MFRg4uBm7aWSoaYo/values/Sheet2?key=AIzaSyCkROWBarEOJ9hQJggyrlUFulOFA4h6AW0&alt=json';
+const url =
+  'https://sheets.googleapis.com/v4/spreadsheets/1S_Apr0HavFCO7H9hKcRjIUrgoT7MFRg4uBm7aWSoaYo/values/Sheet2?key=AIzaSyCkROWBarEOJ9hQJggyrlUFulOFA4h6AW0&alt=json';
 
 // TEMP: This is roughly a copy of https://github.com/tomayac/fugu-showcase/blob/dfc36868e81588add849df4aabb12e8dba5b2d89/index.mjs#L54
 async function run() {
@@ -19,7 +19,7 @@ async function run() {
   const r = await fetch(url);
 
   if (!r.ok) {
-    throw new Error(`Could not fetch fugu showcase entries, status: ${r.status}`);
+    throw new Error(`Could not fetch fugu entries, status: ${r.status}`);
   }
 
   const json = await r.json();
@@ -30,12 +30,12 @@ async function run() {
   }
 
   const values = json.values;
-  const data = values.map((row) => {
+  const data = values.map(row => {
     return {
       timestamp: new Date(row[0]),
       appURL: row[1],
       sourceURL: row[2] || null,
-      usedAPIs: row[3].split(', ').map((api) => {
+      usedAPIs: row[3].split(', ').map(api => {
         return {
           name: api.replace(/\(https.*\)/g, '').trim(),
           url: api.replace(/.*?\((https.*)\)/g, '$1'),
--- site/_data/fuguApis.js
+++ site/_data/fuguApis.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-
 /**
  * @return {Promise<TwitterTweet[]>}
  */
 module.exports = async () => {
-  const fuguFile = path.join(__dirname, '../../external/data/fugu-showcase.json');
-  let fuguItems = /** @type {TwitterTweet[]} */ (
+  const fuguFile = path.join(
+    __dirname,
+    '../../external/data/fugu-showcase.json'
+  );
+  const fuguItems = /** @type {TwitterTweet[]} */ (
     JSON.parse(fs.readFileSync(fuguFile, 'utf-8'))
   );
-
   return fuguItems;
 };
