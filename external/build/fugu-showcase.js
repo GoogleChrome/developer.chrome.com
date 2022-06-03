c4a506be3344b363a24b7ef77c9cec48a84794cb
Thomas Steiner <tomac@google.com>
2022-06-03 17:02:29 +0200

    Start migrating the showcase

--- external/build/fugu-showcase.js
+++ external/build/fugu-showcase.js
@@ -1,5 +1,6 @@
 /**
- * @fileoverview Fetches the most recent tweet from ChromiumDev and writes to storage.
+ * @fileoverview Fetches the Fugu Projects following the data process described
+ * in https://github.com/GoogleChromeLabs/fugu-showcase#data-flow.
  */
 
 require('dotenv').config();
@@ -7,46 +8,74 @@ require('dotenv').config();
 const {default: fetch} = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
-const url =
+const {unfurl} = require('unfurl.js');
+
+const SPREADSHEET_URL =
   'https://sheets.googleapis.com/v4/spreadsheets/1S_Apr0HavFCO7H9hKcRjIUrgoT7MFRg4uBm7aWSoaYo/values/Sheet2?key=AIzaSyCkROWBarEOJ9hQJggyrlUFulOFA4h6AW0&alt=json';
 
-// TEMP: This is roughly a copy of https://github.com/tomayac/fugu-showcase/blob/dfc36868e81588add849df4aabb12e8dba5b2d89/index.mjs#L54
 async function run() {
   if (process.env.CI) {
     return; // do nothing, the fallback data will win
   }
+  try {
+    const response = await fetch(SPREADSHEET_URL);
+    if (!response.ok) {
+      throw new Error(
+        `Could not fetch Fugu entries. Status code was: ${response.status}.`
+      );
+    }
+    const json = await response.json();
+    if (json['errors']) {
+      const error = json['errors'][0];
+      throw new Error(`${error.code}: ${error.message}`);
+    }
 
-  const r = await fetch(url);
+    const values = json.values;
+    const data = values.map(row => {
+      return {
+        timestamp: new Date(row[0]),
+        appURL: row[1],
+        sourceURL: row[2] || null,
+        usedAPIs: row[3].split(', ').map(api => {
+          return {
+            name: api.replace(/\(https.*\)/g, '').trim(),
+            url: api.replace(/.*?\((https.*)\)/g, '$1'),
+          };
+        }),
+        screenshot: '',
+      };
+    });
 
-  if (!r.ok) {
-    throw new Error(`Could not fetch fugu entries, status: ${r.status}`);
+    await Promise.all(
+      data.map(async item => {
+        return unfurl(item.appURL, {oembed: false, compress: true})
+          .then(meta => {
+            item.meta = meta;
+            item.title =
+              meta.open_graph?.title ||
+              meta.twitter_card?.title ||
+              meta.title ||
+              new URL(item.appURL).hostname;
+            item.description =
+              meta.open_graph?.description ||
+              meta.twitter_card?.description ||
+              meta.description ||
+              '';
+          })
+          .catch(err => {
+            item.meta = {};
+            item.title = new URL(item.appURL).hostname;
+            item.description = '';
+          });
+      })
+    );
+
+    const targetFile = path.join(__dirname, '../data/fugu-showcase.json');
+    fs.writeFileSync(targetFile, JSON.stringify(data, null, 2));
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-
-  const json = await r.json();
-
-  if (json['errors']) {
-    const error = json['errors'][0];
-    throw new Error(`${error.code}: ${error.message}`);
-  }
-
-  const values = json.values;
-  const data = values.map(row => {
-    return {
-      timestamp: new Date(row[0]),
-      appURL: row[1],
-      sourceURL: row[2] || null,
-      usedAPIs: row[3].split(', ').map(api => {
-        return {
-          name: api.replace(/\(https.*\)/g, '').trim(),
-          url: api.replace(/.*?\((https.*)\)/g, '$1'),
-        };
-      }),
-      // screenshot: `${fileNamifyURL(row[1])}.${SCREENSHOT_OPTIONS.type}`,
-    };
-  });
-
-  const targetFile = path.join(__dirname, '../data/fugu-showcase.json');
-  fs.writeFileSync(targetFile, JSON.stringify(data, null, 2));
 }
 
 run();
--- site/_data/fuguApis.js
+++ site/_data/fuguApis.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-/**
- * @return {Promise<TwitterTweet[]>}
- */
 module.exports = async () => {
   const fuguFile = path.join(
     __dirname,
     '../../external/data/fugu-showcase.json'
   );
-  const fuguItems = /** @type {TwitterTweet[]} */ (
+  const fuguItems = /** @type {FuguProject[]} */ (
     JSON.parse(fs.readFileSync(fuguFile, 'utf-8'))
   );
-  return fuguItems;
+
+  const availableAPIs = new Set();
+  fuguItems.forEach(item => {
+    item.usedAPIs.forEach(api => {
+      availableAPIs.add(api.name);
+    });
+  });
+
+  return {
+    fuguItems,
+    availableApis: Array.from(availableAPIs.keys()).sort((a, b) =>
+      a.toLowerCase().localeCompare(b.toLowerCase())
+    ),
+  };
 };
