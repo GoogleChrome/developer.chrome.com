1502f7f856f0a09e053189c7e5ebdbfd646481c4
Thomas Steiner <tomac@google.com>
2022-06-22 15:53:42 +0200

    nter#

--- external/build/fugu-showcase.js
+++ external/build/fugu-showcase.js
@@ -19,7 +19,7 @@ const SCREENSHOT_OPTIONS = {
   height: 800,
   overwrite: true,
   delay: 2,
-  type: 'webp',
+  /** @type "webp" | "png" | "jpeg" | undefined' */ type: 'webp',
 };
 
 async function limit(tasks, concurrency) {
