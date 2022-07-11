c4e0359ca9c09422c65a74878cf59519201b4773
aasim1155 <108793097+aasim1155@users.noreply.github.com>
2022-07-11 16:28:28 +0530

    Delete ava.config.js

--- ava.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  files: ['tests/**/*'],
-  // This needs to be turned on to let AVA use power-assert for enhanced
-  // assertion messages.
-  // Read more: https://github.com/avajs/ava/blob/master/docs/03-assertions.md#enhanced-assertion-messages
-  babel: true,
-};
