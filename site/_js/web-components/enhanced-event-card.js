aaf18dce4095f1a91ba468b7b159daaeff9afd92
John <johnrich85@hotmail.com>
2022-09-13 13:27:34 +0100

    Added a web component responsible for enhancing event cards.

---
+++ site/_js/web-components/enhanced-event-card.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { BaseElement } from './base-element'
+
+/* eslint-disable require-jsdoc */
+export class EnhancedEventCard extends BaseElement {
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.card = this.getCardElement()
+    this.detailsButton = this.getButtonElement(this.card)
+
+    this.detailsButton.addEventListener('click', this.toggleEventDetails);
+
+    this.card.classList.remove('no-js');
+  }
+
+  disconnectedCallback () {
+    super.disconnectedCallback();
+  }
+
+  toggleEventDetails = () =>  {
+    // @ts-ignore
+    const visible = this.card.getAttribute('show-details') === "1" ? "0" : "1";
+
+    this.detailsButton.innerHTML = visible === "1" ? 'Hide event details' : 'See event details';
+
+    // @ts-ignore
+    this.card.setAttribute('show-details', visible);
+  }
+
+  getCardElement() {
+    const card = this.querySelector('.event-card');
+
+    if (!card) {
+      throw new Error('Missing event card');
+    }
+
+    return card;
+  }
+
+  getButtonElement(card) {
+    const button = card.querySelector('.event-card__overview button');
+
+    if (!button) {
+      throw new Error('Event card is missing the details button');
+    }
+
+    return button;
+  }
+}
+
+customElements.define('enhanced-event-card', EnhancedEventCard);
--- site/_shortcodes/EventCard.js
+++ site/_shortcodes/EventCard.js
@@ -9,92 +9,59 @@ const pinIcon =  fs.readFileSync(`site/_includes/icons/pin.svg`, 'utf-8');
 
 function EventCard(event) {
   return html`
-    <article class="event-card pad-400 gap-bottom-400">
-        <div class="display-flex">
-          <figure class="event-card__desktop-image gap-right-400 flex-shrink-none">
-              <!--Todo - handle images-->
-              <img src="https://via.placeholder.com/250"
-                   alt="placeholder"
-                   loading="lazy"
-                   width="225"
-                   decoding="async"/>
-          </figure>
+    <enhanced-event-card>
+      <article class="event-card pad-400 no-js" show-details="0">
+          <div class="display-flex">
+            <figure class="event-card__desktop-image gap-right-400 flex-shrink-none">
+                <!--Todo - handle images-->
+                <img src="https://via.placeholder.com/250"
+                     alt="placeholder"
+                     loading="lazy"
+                     width="225"
+                     decoding="async"/>
+            </figure>
 
-          <div class="display-flex direction-column justify-content-between">
-            <div>
-              <h4 class="event-card__title">
-                <figure class="event-card__image gap-right-200 flex-shrink-none">
-                  <!--Todo - handle images-->
-                  <img src="https://via.placeholder.com/250"
-                       alt="placeholder"
-                       loading="lazy"
-                       width="38"
-                       decoding="async"/>
-                </figure>
+            <div class="event-card__overview display-flex direction-column justify-content-between">
+              <div>
+                <h4 class="event-card__title">
+                  <figure class="event-card__image gap-right-200 flex-shrink-none">
+                    <!--Todo - handle images-->
+                    <img src="https://via.placeholder.com/250"
+                         alt="placeholder"
+                         loading="lazy"
+                         width="38"
+                         decoding="async"/>
+                  </figure>
 
-                ${event.title}
-              </h4>
+                  ${event.title}
+                </h4>
 
-              <p>
-                ${event.summary}
-              </p>
-
-              <div class="event-card__meta display-flex gap-bottom-400">
-                <p class="display-flex align-center gap-right-500">
-                    ${pinIcon}
-                    ${event.location}
+                <p>
+                  ${event.summary}
                 </p>
 
-                <p class="display-flex align-center">
-                    ${calendarIcon}
-                    ${formatDateShort(event.date)}
-                </p>
+                <div class="event-card__meta display-flex gap-bottom-400">
+                  <p class="display-flex align-center gap-right-500">
+                      ${pinIcon}
+                      ${event.location}
+                  </p>
+
+                  <p class="display-flex align-center">
+                      ${calendarIcon}
+                      ${formatDateShort(event.date)}
+                  </p>
+                </div>
               </div>
+
+              <button class="material-button button-filled button-round color-bg">
+                See event details
+              </a>
             </div>
-
-            <a class="material-button bg-blue-medium button-filled button-round color-bg"
-               href="/docs/"
-            >
-              See event details
-            </a>
           </div>
-        </div>
 
-        <div class="gap-top-400 display-grid grid-cols-1 grid-gap-400 lg:grid-cols-2">
-            ${event.talks.map((talk) => (
-              EventParticipantCard(html`
-                <h5 class="event-card__title gap-bottom-300 display-flex align-center">
-                  <!--Todo - handle images-->
-                  <img alt="Milica Mihajlija"
-                       class="flex-shrink-none height-600 width-600 rounded-full gap-right-300"
-                       decoding="async"
-                       height="40"
-                       width="40"
-                       loading="lazy"
-                       sizes="40px"
-                       src="https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format"
-                       srcset="https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=40 40w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=46 46w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=52 52w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=59 59w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=68 68w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=77 77w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=88 88w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=100 100w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=114 114w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=120 120w"
-                    />
-                    ${talk.speaker}
-                  </h5>
-
-                  <p class="event-card__sub-title gap-bottom-200">${talk.title}</p>
-
-                  <div class="gap-bottom-200">
-                    <p>${talk.description}</p>
-                  </div>
-
-                  <a href="/todo"
-                     class="surface color-secondary-text decoration-none hairline rounded-lg tag-pill type--label weight-regular">
-                    ${talk.topic}
-                  </a>
-
-                  <p class="display-flex align-center gap-top-300">${clockIcon} ${talk.time}</p>
-              `)
-            ))}
-
-            ${event.participant_groups.map((group) => (
-              EventParticipantCard(html`
+          <div class="event-card__details gap-top-400 grid-cols-1 grid-gap-400 lg:grid-cols-2">
+              ${event.talks.map((talk) => (
+                EventParticipantCard(html`
                   <h5 class="event-card__title gap-bottom-300 display-flex align-center">
                     <!--Todo - handle images-->
                     <img alt="Milica Mihajlija"
@@ -107,34 +74,67 @@ function EventCard(event) {
                          src="https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format"
                          srcset="https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=40 40w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=46 46w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=52 52w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=59 59w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=68 68w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=77 77w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=88 88w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=100 100w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=114 114w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=120 120w"
                       />
-                      ${group.participants.length === 1 ? group.participants[0].name : 'Multiple participants'}
+                      ${talk.speaker}
                     </h5>
 
-                    ${group.participants.length > 1 && (
-                      html`
-                        <p class="event-card__sub-title gap-bottom-200">Participants</p>
-                        <p class="gap-bottom-300">${group.participants.map(p => p.name).join(', ')}</p>
-                      `
-                    )}
-
-                    <p class="event-card__sub-title gap-bottom-200">Participant details</p>
+                    <p class="event-card__sub-title gap-bottom-200">${talk.title}</p>
 
                     <div class="gap-bottom-200">
-                      <p>${group.description}</p>
+                      <p>${talk.description}</p>
                     </div>
 
-                    ${group.topic && (
-                      html`
-                        <a href="/todo"
-                           class="surface color-secondary-text decoration-none hairline rounded-lg tag-pill type--label weight-regular">
-                          ${group.topic}
-                        </a>
-                      `
-                    )}
+                    <a href="/todo"
+                       class="surface color-secondary-text decoration-none hairline rounded-lg tag-pill type--label weight-regular">
+                      ${talk.topic}
+                    </a>
+
+                    <p class="display-flex align-center gap-top-300">${clockIcon} ${talk.time}</p>
                 `)
-            ))}
-        </div>
-    </article>
+              ))}
+
+              ${event.participant_groups.map((group) => (
+                EventParticipantCard(html`
+                    <h5 class="event-card__title gap-bottom-300 display-flex align-center">
+                      <!--Todo - handle images-->
+                      <img alt="Milica Mihajlija"
+                           class="flex-shrink-none height-600 width-600 rounded-full gap-right-300"
+                           decoding="async"
+                           height="40"
+                           width="40"
+                           loading="lazy"
+                           sizes="40px"
+                           src="https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format"
+                           srcset="https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=40 40w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=46 46w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=52 52w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=59 59w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=68 68w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=77 77w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=88 88w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=100 100w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=114 114w, https://wd.imgix.net/image/vgdbNJBYHma2o62ZqYmcnkq3j0o1/lLsarI5KPmS0kwK9L4Ea.jpeg?auto=format&amp;w=120 120w"
+                        />
+                        ${group.participants.length === 1 ? group.participants[0].name : 'Multiple participants'}
+                      </h5>
+
+                      ${group.participants.length > 1 && (
+                        html`
+                          <p class="event-card__sub-title gap-bottom-200">Participants</p>
+                          <p class="gap-bottom-300">${group.participants.map(p => p.name).join(', ')}</p>
+                        `
+                      )}
+
+                      <p class="event-card__sub-title gap-bottom-200">Participant details</p>
+
+                      <div class="gap-bottom-200">
+                        <p>${group.description}</p>
+                      </div>
+
+                      ${group.topic && (
+                        html`
+                          <a href="/todo"
+                             class="surface color-secondary-text decoration-none hairline rounded-lg tag-pill type--label weight-regular">
+                            ${group.topic}
+                          </a>
+                        `
+                      )}
+                  `)
+              ))}
+          </div>
+      </article>
+    </enhanced-event-card>
   `;
 }
 
