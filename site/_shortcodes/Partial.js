4673b70e0fd62e877a804f11e877ad564326e6ec
Matthias Rohmer <mrohmer@google.com>
2022-09-15 10:31:05 +0200

    feat: build nunjucks shortcode for partials

--- site/_shortcodes/Partial.js
+++ site/_shortcodes/Partial.js
@@ -1,15 +1,40 @@
-const { findByUrl } = require('../_data/lib/find');
-const { getLocalizedPaths, stripLocale } = require('../_filters/urls');
+const path = require('path');
+
+const {findByFilePath} = require('../_data/lib/find');
+
+const defaultLocale = 'en';
+const basePath = '_partials';
 
 /**
- * Outputs a list of <a href> tags with alternate language versions
- * for the given url.
- * @param {string} url Current page url
- * @param {EleventyCollectionObject} collections Eleventy collections object
- * @param {string} locale current locale
+ * Returns the rendered content of a page living in the partials collection
+ * @param {string} partialPath Partial path
  */
-function Partial(path, collections, locale = 'en') {
-  return path;
+async function Partial(partialPath) {
+  const locale = this.ctx.locale || defaultLocale;
+
+  partialPath = path.join('/', basePath, partialPath);
+  // filePathStems for eleventy end at the directory name, w/o a trailing slash
+  // Therefore make sure there aren't any file extensions or index stubs
+  partialPath = partialPath.replace(/(index)?.(md|njk|html)$/, '');
+
+  const partial = findByFilePath(
+    this.ctx.collections.partials,
+    partialPath,
+    locale
+  );
+  if (!partial) {
+    console.warn(`No partial found at ${partialPath}`);
+    return '';
+  }
+
+  // Proactively render the template to prevent 11ty from complaining
+  // that we are accessing a rendered template (partial.templateContent)
+  // before it has done all it's business
+  const template = partial.template;
+  template.wrapWithLayouts = false;
+
+  const templateContent = await template.render(this.ctx);
+  return templateContent;
 }
 
-module.exports = { Partial };
+module.exports = {Partial};
