3c5f0a75c5e86bd4a74d8e515d4245874b33b9bf
Ewa Gasperowicz <devnook@gmail.com>
2022-09-21 16:17:05 +0200

    lint

--- site/_data/workboxApiModules.js
+++ site/_data/workboxApiModules.js
@@ -21,12 +21,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const workboxModules = process.env.ELEVENTY_IGNORE_EXTENSIONS ? fs
-  .readdirSync(path.join(__dirname, '../en/docs/workbox/modules'), {
-    withFileTypes: true,
-  })
-  .filter(d => d.isDirectory())
-  .map(d => d.name) : [];
+const workboxModules = process.env.ELEVENTY_IGNORE_EXTENSIONS
+  ? fs
+      .readdirSync(path.join(__dirname, '../en/docs/workbox/modules'), {
+        withFileTypes: true,
+      })
+      .filter(d => d.isDirectory())
+      .map(d => d.name)
+  : [];
 
 module.exports = () => {
   if (process.env.ELEVENTY_IGNORE_EXTENSIONS) {
