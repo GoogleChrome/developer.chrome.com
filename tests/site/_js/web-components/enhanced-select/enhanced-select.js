a2ff3d58b08607641707c829e8a7edf029fc51b2
John <johnr@tokyo.uk>
2022-10-31 13:24:12 +0000

    Adds the remaining tests for the enhanced-select WC

--- tests/site/_js/web-components/enhanced-select/enhanced-select.js
+++ tests/site/_js/web-components/enhanced-select/enhanced-select.js
@@ -77,31 +77,238 @@ test(
     await addPageScript(page, '_enhanced-select.js');
 
     const massSelect = await page.$('enhanced-select li:last-child');
+
+    let dispatchedChangeEvent = false;
+
+    await page.exposeFunction('onCustomEvent', async () => {
+      dispatchedChangeEvent = true;
+    });
+
+    await page.evaluate(() => {
+      document
+        .querySelector('enhanced-select')
+        .addEventListener('change', () => window.onCustomEvent());
+    });
+
     await massSelect.click();
 
     const value = await page.evaluate(() => {
       return document.querySelector('enhanced-select').getAttribute('value');
     });
 
-    //todo - detect event
-
     t.deepEqual(JSON.parse(value), ['2']);
+    t.is(dispatchedChangeEvent, true);
   }
 );
 
 test('enhanced-select: supports multiselect', withPage, async (t, page) => {
-  t.is(1, 2);
-});
+  await page.setContent(html`
+    <enhanced-select>
+      <select name="test" multiple>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </enhanced-select>
+  `);
 
-test('enhanced-select: supports tab navigation', withPage, async (t, page) => {
-  t.is(1, 2);
+  await addPageScript(page, '_enhanced-select.js');
+
+  const firstOption = await page.$('enhanced-select li:first-child');
+  const lastOption = await page.$('enhanced-select li:last-child');
+
+  await lastOption.click();
+  await firstOption.click();
+
+  const value = await page.evaluate(() => {
+    return document.querySelector('enhanced-select').getAttribute('value');
+  });
+
+  t.deepEqual(JSON.parse(value), ['1', '3']);
 });
 
 test(
-  'enhanced-select: supports keyboard navigation',
+  'enhanced-select: can be opened/closed with keyboard',
   withPage,
   async (t, page) => {
-    t.is(1, 2);
+    await page.setContent(html`
+      <enhanced-select>
+        <select name="test" multiple>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </enhanced-select>
+    `);
+
+    await addPageScript(page, '_enhanced-select.js');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+
+    const open1 = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').open;
+    });
+
+    await page.keyboard.press('Escape');
+
+    const open2 = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').open;
+    });
+
+    t.is(open1, true);
+    t.is(open2, false);
+  }
+);
+
+test('enhanced-select: supports tab navigation', withPage, async (t, page) => {
+  await page.setContent(html`
+    <enhanced-select>
+      <select name="test" multiple>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </enhanced-select>
+  `);
+
+  await addPageScript(page, '_enhanced-select.js');
+
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Enter');
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Tab');
+
+  const activeOpt = await page.evaluate(() => {
+    return document.querySelector('enhanced-select').focusedIndex;
+  });
+
+  t.is(activeOpt, 2);
+});
+
+test(
+  'enhanced-select: closes after tabbing through to next element',
+  withPage,
+  async (t, page) => {
+    await page.setContent(html`
+      <enhanced-select>
+        <select name="test" multiple>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </enhanced-select>
+    `);
+
+    await addPageScript(page, '_enhanced-select.js');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+
+    const open = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').open;
+    });
+
+    t.is(open, false);
+  }
+);
+
+test(
+  'enhanced-select: supports arrow navigation',
+  withPage,
+  async (t, page) => {
+    await page.setContent(html`
+      <enhanced-select>
+        <select name="test" multiple>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </enhanced-select>
+    `);
+
+    await addPageScript(page, '_enhanced-select.js');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+
+    const activeOpt = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').focusedIndex;
+    });
+
+    t.is(activeOpt, 1);
+  }
+);
+
+test(
+  'enhanced-select: supports pgup/down selection',
+  withPage,
+  async (t, page) => {
+    await page.setContent(html`
+      <enhanced-select>
+        <select name="test" multiple>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </enhanced-select>
+    `);
+
+    await addPageScript(page, '_enhanced-select.js');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('PageDown');
+
+    const activeOpt1 = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').focusedIndex;
+    });
+
+    await page.keyboard.press('PageUp');
+
+    const activeOpt2 = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').focusedIndex;
+    });
+
+    t.is(activeOpt1, 2);
+    t.is(activeOpt2, 0);
+  }
+);
+
+test(
+  'enhanced-select: supports keyboard selection',
+  withPage,
+  async (t, page) => {
+    await page.setContent(html`
+      <enhanced-select>
+        <select name="test" multiple>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </enhanced-select>
+    `);
+
+    await addPageScript(page, '_enhanced-select.js');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Enter');
+
+    const value = await page.evaluate(() => {
+      return document.querySelector('enhanced-select').getAttribute('value');
+    });
+
+    t.deepEqual(JSON.parse(value), ['2']);
   }
 );
 
@@ -109,6 +316,27 @@ test(
   'enhanced-select: interfaces with forms as expected',
   withPage,
   async (t, page) => {
-    t.is(1, 2);
+    await page.setContent(html`
+      <form>
+        <enhanced-select>
+          <select name="test" multiple>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </enhanced-select>
+      </form>
+    `);
+
+    await addPageScript(page, '_enhanced-select.js');
+
+    const firstOption = await page.$('enhanced-select li:first-child');
+    await firstOption.click();
+
+    const formData = await page.evaluate(() => {
+      return new FormData(document.querySelector('form')).getAll('test');
+    });
+
+    t.deepEqual(formData, ['1']);
   }
 );
