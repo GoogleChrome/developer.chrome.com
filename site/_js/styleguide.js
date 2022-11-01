91b1945193e9fdbb11d7503255b7cd3488a453f8
John Richardson <johnr@tokyo.uk>
2022-11-01 09:29:02 +0000

    Adds Checkbox group (#3998)
    * Checkbox group WC: WIP
    
    * Checkbox group WC: added 'show more' button
    
    * Checkbox group WC bugfix: prevent show more button being shown if show > num items.
    
    * Flattens the i18n yaml object to contain only translations from given locale
    
    * Checkbox group: Localisation
    
    * Moved CheckboxGroup out of the main entrypoints and into their event specific counterparts.
    
    * Added a new scss entrypoint for the styleguide page so that non-global styles can be used where necessary.
    
    * Updated style import after cherry-pick
    
    * Added js entrypoint for the styleguide
    
    * Added checkbox group to the styleguide entrypoint.
    
    * Fixes a bug whereby the 'allSelected' property was not being computed on init
    
    * Adds puppeteer and rollup capabilities to the test suite so that web components can be tested.
    
    * Adds checkbox-group tests
    
    * Adds the new test dist dir to ignore list
    
    * Moves WC tests up one level.
    
    * Fixed indentation
    
    * Moves puppeteer/rollup to the base tests directory as they're needed for other test types.
    
    * Moves test to the unit tests directory.
    
    * Ignores the new dist dir.
    
    Co-authored-by: Ewa <devnook@gmail.com>

--- site/_js/styleguide.js
+++ site/_js/styleguide.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import './web-components/enhanced-event-card';
-import './web-components/truncate-text';
 import './web-components/checkbox-group';
