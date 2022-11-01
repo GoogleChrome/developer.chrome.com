378350eef43d79ce3cfee9fe12f900b88d7fc830
John <johnr@tokyo.uk>
2022-11-01 09:38:38 +0000

    Fixes merge issue

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,8 +1,10 @@
 import originalConfig from '../rollup.config';
 
 const testConfig = Object.assign({}, originalConfig(), {
-  input: ['site/_js/web-components/enhanced-select/_enhanced-select'],
-  input: ['site/_js/web-components/checkbox-group/_checkbox-group'],
+  input: [
+    'site/_js/web-components/enhanced-select/_enhanced-select',
+    'site/_js/web-components/checkbox-group/_checkbox-group',
+  ],
 });
 
 export default testConfig;
