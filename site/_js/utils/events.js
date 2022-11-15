8a401f689cc2763f28f25539a4c74281e7294151
John <johnr@tokyo.uk>
2022-11-15 14:51:51 +0000

    Gives utils more explicit names

--- site/_js/utils/events.js
+++ site/_js/utils/events.js
@@ -5,10 +5,10 @@ const isPastEvent = event => {
   return new Date(event.date).getTime() < startOfDay.getTime();
 };
 
-const sortDesc = (a, b) =>
+const sortDescByTime = (a, b) =>
   new Date(b.date).getTime() - new Date(a.date).getTime();
 
-const sortAsc = (a, b) =>
+const sortAscByTime = (a, b) =>
   new Date(a.date).getTime() - new Date(b.date).getTime();
 
-module.exports = {isPastEvent, sortDesc, sortAsc};
+module.exports = {isPastEvent, sortDescByTime, sortAscByTime};
