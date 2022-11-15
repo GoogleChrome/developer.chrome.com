5b541efe53d94fddcc71d2427aa3f695a62fbcae
John <johnr@tokyo.uk>
2022-11-15 13:59:44 +0000

    Removes reference to recently deleted shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,6 @@ const {Codepen} = require('webdev-infra/shortcodes/Codepen');
 const {Details} = require('./site/_shortcodes/Details');
 const {DetailsSummary} = require('./site/_shortcodes/DetailsSummary');
 const {Empty} = require('./site/_shortcodes/Empty');
-const {EventCard} = require('./site/_shortcodes/EventCard');
 const {IFrame} = require('./site/_shortcodes/IFrame');
 const {Glitch} = require('./site/_shortcodes/Glitch');
 const {Hreflang} = require('./site/_shortcodes/Hreflang');
@@ -142,7 +141,6 @@ module.exports = eleventyConfig => {
   eleventyConfig.addPairedShortcode('Column', Column);
   eleventyConfig.addPairedShortcode('Compare', Compare);
   eleventyConfig.addPairedShortcode('CompareCaption', CompareCaption);
-  eleventyConfig.addShortcode('EventCard', EventCard);
   eleventyConfig.addPairedShortcode('Aside', Aside);
   eleventyConfig.addPairedShortcode('Label', Label);
   eleventyConfig.addShortcode('LanguageList', LanguageList);
