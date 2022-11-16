c12e7dfc63dbdbe94fbfde3bded09e0a03b7cb34
John <johnr@tokyo.uk>
2022-11-16 12:44:16 +0000

    Fixes failing tests

--- tests/site/_js/misc/load-more/_load-more.js
+++ tests/site/_js/misc/load-more/_load-more.js
@@ -14,6 +14,7 @@ import {loadMore} from '../../../../../site/_js/misc/load-more';
           '<div>5</div>',
         ];
       },
+      () => {},
       {
         take: 5,
         skip: 0,
@@ -37,6 +38,7 @@ import {loadMore} from '../../../../../site/_js/misc/load-more';
           '<div>5</div>',
         ];
       },
+      () => {},
       {
         take: 5,
         skip: 0,
