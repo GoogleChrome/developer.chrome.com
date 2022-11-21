9968470a2c6de875f9dc1167905e40676b4b48c9
John <johnr@tokyo.uk>
2022-11-21 11:58:39 +0000

    - Updates the test suite's rollup config, placing visual bundles in the visual dist dir. - Adds main entrypoint.

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,14 +1,25 @@
 import originalConfig from '../rollup.config';
 
+const config = originalConfig();
+
 const inputs = [
   'site/_js/web-components/enhanced-select/_enhanced-select',
   'site/_js/web-components/checkbox-group/_checkbox-group',
+  'visual/fixtures/site/_js/main',
   'site/_js/misc/load-more/_load-more',
 ];
 
 const configs = inputs.map(input => {
-  return Object.assign({}, originalConfig(), {
-    input: input,
+  const output = input.startsWith('visual/')
+    ? {
+        dir: 'visual/fixtures/dist/js',
+        format: 'esm',
+      }
+    : config.output;
+
+  return Object.assign({}, config, {
+    input,
+    output,
   });
 });
 
