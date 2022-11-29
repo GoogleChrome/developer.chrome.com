fa63c58dbc3470b63a80f11cd3465ceca0a29b33
John <johnr@tokyo.uk>
2022-11-29 09:42:42 +0000

    Adds the new blog-landing template as a percy target

--- .percy.js
+++ .percy.js
@@ -1,6 +1,7 @@
 const SNAPSHOTS = [
   '/en/content-types/author-individual/index.html',
   '/en/page-types/authors-landing/index.html',
+  '/en/page-types/blog-landing/index.html',
   '/en/content-types/blog-post/index.html',
 ];
 
