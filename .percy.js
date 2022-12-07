d83bd6a3c4d0f76282416e5caa3b608b0260bb67
John <johnr@tokyo.uk>
2022-12-07 14:51:25 +0000

    Adds the docs-landing page as a percy target

--- .percy.js
+++ .percy.js
@@ -1,6 +1,7 @@
 const SNAPSHOTS = [
   '/en/content-types/author-individual/index.html',
   '/en/content-types/blog-post/index.html',
+  '/en/content-types/docs-landing/index.html',
 ];
 
 module.exports = {
