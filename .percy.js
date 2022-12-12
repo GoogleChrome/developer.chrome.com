aeacff700d1567ad961ab9c37d37d2403c128b57
John Richardson <johnr@tokyo.uk>
2022-12-12 09:48:39 +0000

    Adds a sample docs-landing page as a percy target (#4542)
    * Prevent the new sample pages from being indexed or being included in search results
    
    * Adds the docs-landing sample page
    
    * Adds the docs-landing page as a percy target

--- .percy.js
+++ .percy.js
@@ -2,6 +2,7 @@ const SNAPSHOTS = [
   '/en/content-types/author-individual/index.html',
   '/en/content-types/blog-landing/index.html',
   '/en/content-types/blog-post/index.html',
+  '/en/content-types/docs-landing/index.html',
 ];
 
 module.exports = {
