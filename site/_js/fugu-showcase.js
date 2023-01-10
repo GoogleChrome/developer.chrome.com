e06162db75620811d2ae8573e445f19f2b08b2e5
Maya Janthong <maya@tokyo.uk>
2023-01-10 15:55:16 +0700

    style: fix scss lint error

--- site/_js/fugu-showcase.js
+++ site/_js/fugu-showcase.js
@@ -17,41 +17,49 @@
 let filterValue = [];
 
 /**
-* @param searchText
-* @param selectedValue
-*/
+ * @param {string} searchText
+ * @param {string[]} selectedValue
+ */
 function displayShowcase(searchText, selectedValue) {
-  const articles = /** @type {NodeListOf<HTMLElement>} */ (document.querySelectorAll('.fugu-card[data-used-apis]'));
+  const articles = /** @type {NodeListOf<HTMLElement>} */ (
+    document.querySelectorAll('.fugu-card[data-used-apis]')
+  );
+
   if (!searchText.length && !selectedValue.length) {
-    articles.forEach((article) => {
+    articles.forEach(article => {
       article.style.display = 'block';
     });
   } else {
-    articles.forEach((article) => {
+    articles.forEach(article => {
       article.style.display = 'none';
     });
   }
 
   const filter = selectedValue.join(' ');
   const container = document.querySelector('.fugu-container');
-  const matchingFilterArticles = /** @type {NodeListOf<HTMLElement>} */ (container?.querySelectorAll(`article[data-used-apis*="${filter}"]`));
-  const matchingSearchArticles = /** @type {NodeListOf<HTMLElement>} */ (container?.querySelectorAll(`article[data-name*="${searchText}"]`));
+  const matchingFilterArticles = /** @type {NodeListOf<HTMLElement>} */ (
+    container?.querySelectorAll(`article[data-used-apis*="${filter}"]`)
+  );
 
-  matchingSearchArticles?.forEach((article) => {
+  const matchingSearchArticles = /** @type {NodeListOf<HTMLElement>} */ (
+    container?.querySelectorAll(`article[data-name*="${searchText}"]`)
+  );
+
+  matchingSearchArticles?.forEach(article => {
     if (!matchingFilterArticles?.length) article.style.display = 'block';
     else {
-      matchingFilterArticles.forEach((filterArticle) => {
+      matchingFilterArticles.forEach(filterArticle => {
         if (article === filterArticle) {
           filterArticle.style.display = 'block';
         }
       });
     }
   });
-  
-  matchingFilterArticles?.forEach((article) => {
+
+  matchingFilterArticles?.forEach(article => {
     if (!matchingSearchArticles?.length) article.style.display = 'block';
     else {
-      matchingSearchArticles.forEach((searchArticle) => {
+      matchingSearchArticles.forEach(searchArticle => {
         if (article === searchArticle) {
           searchArticle.style.display = 'block';
         }
@@ -60,64 +68,99 @@ function displayShowcase(searchText, selectedValue) {
   });
 }
 
-const searchByAppNameElement = /** @type {HTMLElement} */ (document.querySelector('input.search-apps'));
-searchByAppNameElement.addEventListener('input', (e) => {
-  const articles = /** @type {NodeListOf<HTMLElement>} */ (document.querySelectorAll('.fugu-card[data-used-apis]'));
-  articles.forEach((article) => {
+const searchByAppNameElement = /** @type {HTMLElement} */ (
+  document.querySelector('input.search-apps')
+);
+
+searchByAppNameElement.addEventListener('input', e => {
+  const articles = /** @type {NodeListOf<HTMLElement>} */ (
+    document.querySelectorAll('.fugu-card[data-used-apis]')
+  );
+
+  articles.forEach(article => {
     article.style.display = 'none';
   });
 
   const value = /** @type {HTMLInputElement} */ (e.target).value.toLowerCase();
-  displayShowcase(value, filterValue)
+  displayShowcase(value, filterValue);
 });
 
-const searchByApiElement = /** @type {HTMLElement} */ (document.querySelector('.search-apis'));
-searchByApiElement.addEventListener('click', (e) => {
+const searchByApiElement = /** @type {HTMLElement} */ (
+  document.querySelector('.search-apis')
+);
+
+searchByApiElement.addEventListener('click', () => {
   const selections = document.querySelectorAll('li.button[selected]');
   const filterPills = document.querySelectorAll('span.tag-pill');
 
-  const selectedOptions = []
-  selections.forEach((option) => {
-    return selectedOptions.push(option?.textContent?.trim())
-  })
-  filterPills.forEach((pill) => {
+  const selectedOptions = [];
+  selections.forEach(option => {
+    return selectedOptions.push(option?.textContent?.trim());
+  });
+  filterPills.forEach(pill => {
     if (selectedOptions.includes(pill?.textContent?.trim())) {
-      const value = pill?.textContent?.trim().toLowerCase().split(' ').join('-');
+      const value = pill?.textContent
+        ?.trim()
+        .toLowerCase()
+        .split(' ')
+        .join('-');
 
       if (!filterValue.includes(value)) {
         filterValue.push(value);
-        const searchValue = /** @type {HTMLInputElement} */ (searchByAppNameElement).value?.toLowerCase();
-        displayShowcase(searchValue, filterValue)
+        const searchValue = /** @type {HTMLInputElement} */ (
+          searchByAppNameElement
+        ).value?.toLowerCase();
+
+        displayShowcase(searchValue, filterValue);
       }
 
       pill.removeAttribute('hidden');
     }
-  })
+  });
 });
 
 const closeButtonPills = document.querySelectorAll('span.tag-pill button');
 closeButtonPills.forEach(button => {
   button.addEventListener('click', () => {
-    /** @type {HTMLElement} */ (button.parentNode).setAttribute('hidden', 'true');
+    /** @type {HTMLElement} */ (button.parentNode).setAttribute(
+      'hidden',
+      'true'
+    );
 
-    const enhancedOption = document.querySelectorAll('.enhanced-select__options li');
-    const enhancedSelect = document.querySelector('enhanced-select.search-apis');
+    const enhancedOption = document.querySelectorAll(
+      '.enhanced-select__options li'
+    );
+    const enhancedSelect = document.querySelector(
+      'enhanced-select.search-apis'
+    );
+
     enhancedOption.forEach(option => {
-      if (option.textContent?.trim() === /** @type {HTMLElement} */ (button.parentNode).textContent?.trim()) {
+      if (
+        option.textContent?.trim() ===
+        /** @type {HTMLElement} */ (button.parentNode).textContent?.trim()
+      ) {
         option.removeAttribute('selected');
-        /** @type {HTMLElement} */ enhancedSelect?.setAttribute('value', '[]')
+        /** @type {HTMLElement} */ enhancedSelect?.setAttribute('value', '[]');
       }
     });
 
-    const value = button?.textContent?.trim().toLowerCase().split(' ').join('-');
-    filterValue = filterValue.filter(v => v !== value)
-    
-    const searchValue = /** @type {HTMLInputElement} */ (searchByAppNameElement).value.toLowerCase();
-    displayShowcase(searchValue, filterValue)
+    const value = button?.textContent
+      ?.trim()
+      .toLowerCase()
+      .split(' ')
+      .join('-');
+
+    filterValue = filterValue.filter(v => v !== value);
+
+    const searchValue = /** @type {HTMLInputElement} */ (
+      searchByAppNameElement
+    ).value.toLowerCase();
+
+    displayShowcase(searchValue, filterValue);
   });
 });
 
-window.addEventListener('keydown', (e) => {
+window.addEventListener('keydown', e => {
   if (e.key === 'f' && e.metaKey) {
     e.preventDefault();
     searchByAppNameElement.focus();
