e90cefb6b704dedeaf9475d65506fce0cecd38eb
Matthias Rohmer <mrohmer@google.com>
2023-01-16 17:10:25 +0100

    feat: add task for clean up stale staging environments

--- gulp-tasks/stageGitHub/index.js
+++ gulp-tasks/stageGitHub/index.js
@@ -1,9 +1,11 @@
 const {determineDeploymentType} = require('./determineDeploymentType');
 const {awaitGoogleCloudBuild} = require('./awaitGoogleCloudBuild');
 const {buildAnnouncementComment} = require('./buildAnnouncementComment');
+const {cleanUpGoogleCloud} = require('./cleanUpGoogleCloud');
 
 module.exports = {
   determineDeploymentType,
   awaitGoogleCloudBuild,
   buildAnnouncementComment,
+  cleanUpGoogleCloud
 };
