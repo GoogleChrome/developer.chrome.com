fbfe2c65a3d8f8ccd64a28aaf0171fa8e718a553
simgobb <sim.gobb@gmail.com>
2023-02-17 17:28:14 +0100

    refactor: changed structure of unique href filter

--- site/_filters/ensureUniqueHref.js
+++ site/_filters/ensureUniqueHref.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * @fileoverview This makes sure the ids of page sections are unique
  *
@@ -12,25 +28,22 @@ const pageIds = [];
 
 /**
  * @param {string} id of the section
- * @param {string} url of the page
+ * @this {any} The eleventy context
  * @return {string} unique id for the section
  */
 
-function ensureUniqueHref(id, url) {
-  const idUrl = `${url}#${id}`;
+function ensureUniqueHref(id) {
+  const key = `${this.ctx.page.url}#${id}`;
 
-  if (pageIds.includes(idUrl)) {
-    const count = pageIds.filter(item => {
-      return item === idUrl;
-    }).length;
+  if (!pageIds.includes(key)) {
+    pageIds.push(key);
 
-    pageIds.push(idUrl);
-
-    return `${id}-${count}`;
+    return id;
   }
 
-  pageIds.push(idUrl);
-  return id;
+  throw new Error(
+    `There is another element with the ID ${id} on the page ${this.ctx.page.url}. Please make sure all IDs are unique.`
+  );
 }
 
 module.exports = {
