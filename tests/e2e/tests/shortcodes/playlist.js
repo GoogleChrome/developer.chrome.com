e9aff7f4a0d7741def8e84d82c1f506f7d004480
simgobb <sim.gobb@gmail.com>
2023-02-27 18:02:04 +0100

    test: added tests to additional elements of the component

--- tests/e2e/tests/shortcodes/playlist.js
+++ tests/e2e/tests/shortcodes/playlist.js
@@ -23,7 +23,20 @@ test('renders a playlist', async t => {
   const $ = await getPageByUrl('en/shortcodes/playlist/default');
 
   t.assert($('.playlist').length === 1);
+
+  t.assert($('.playlist__thumbnail > img').attr('src') !== '');
+
+  t.assert($('.playlist__play-all > a').attr('href') !== '');
+
   t.assert($('.playlist__name > a').text().trim().length !== 0);
+  t.assert($('.playlist__name > a').attr('href') !== '');
+
   t.assert($('.playlist__channel-name').text().trim().length !== 0);
+  t.assert($('.playlist__channel-icon > img').attr('src') !== '');
+  t.assert($('.playlist__channel-subscribe > a').attr('href') !== '');
+
   t.assert($('.playlist__videos').length !== 0);
+  t.assert($('.playlist-video__number').text().trim().length !== 0);
+  t.assert($('.playlist-video__content > a').attr('href') !== '');
+  t.assert($('.playlist-video__thumbnail > img').attr('src') !== '');
 });
