811853e40ec404558a048ef4aec3a50798559d0d
simgobb <sim.gobb@gmail.com>
2023-03-29 10:39:44 +0200

    refactor: pre-prepared the urls to keep the component cleaner

--- site/_data/youtubePlaylists.js
+++ site/_data/youtubePlaylists.js
@@ -1,5 +1,25 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
+const YOUTUBE_BASE_URL = 'https://www.youtube.com/';
+
+/**
+ * Pulls a list of playlists
+ *
+ * @return {promise} A promise that resolves in an array containing all the playlists
+ */
+async function getPlaylistData() {
+  let playlists;
+  const playlistsFile = path.join(
+    __dirname,
+    '../../external/data/youtube-playlist.json'
+  );
+  try {
+    playlists = JSON.parse(await fs.readFile(playlistsFile, 'utf-8'));
+  } catch (error) {
+    console.error('Error while reading the playlist data', error);
+  }
+  return playlists;
+}
 
 /**
  * Filters the channels data to retrieve the spefic channel info for each playlist
@@ -8,7 +28,9 @@ const path = require('path');
  * @returns {object}
  */
 function getChannelData(channelId, channels) {
-  return channels.filter(channel => channel.id === channelId)[0];
+  const channel = channels.filter(channel => channel.id === channelId)[0];
+  channel.subscribe = `${YOUTUBE_BASE_URL}/channel/${channel.id}?sub_confirmation=1`;
+  return channel;
 }
 
 /**
@@ -27,16 +49,47 @@ function formatDate(date) {
   });
 }
 
+/**
+ * Builds the video url and adds it to each video object
+ * @param {object[]} videos
+ * @param {string} playlistId
+ * @returns {object[]}
+ */
+function formatVideos(videos, playlistId) {
+  videos.forEach(video => {
+    video.url = `${YOUTUBE_BASE_URL}watch?v=${video.id}&list=${playlistId}`;
+  });
+  return videos;
+}
+
+/**
+ * Builds the playlist url
+ * @param {string} playlistId
+ * @returns {string}
+ */
+function getPlaylistUrl(playlistId) {
+  return `${YOUTUBE_BASE_URL}playlist?list=${playlistId}`;
+}
+
+/**
+ * Builds the url for the "Play All" button
+ * @param {string} playlistId
+ * @param {string} firstVideoUrl
+ * @returns {string}
+ */
+function getPlayAllUrl(playlistId, firstVideoUrl) {
+  return `${YOUTUBE_BASE_URL}watch?v=${firstVideoUrl}&list=${playlistId}`;
+}
+
 module.exports = async () => {
-  const playlistsFile = path.join(
-    __dirname,
-    '../../external/data/youtube-playlist.json'
-  );
-  const data = JSON.parse(fs.readFileSync(playlistsFile, 'utf-8'));
+  const data = await getPlaylistData();
 
   data.playlists.forEach(playlist => {
+    playlist.url = getPlaylistUrl(playlist.id);
+    playlist.playAll = getPlayAllUrl(playlist.id, playlist.videos[0].id);
     playlist.updated = formatDate(playlist.updated);
     playlist.channel = getChannelData(playlist.channel, data.channels);
+    playlist.videos = formatVideos(playlist.videos, playlist.id);
   });
 
   return data.playlists;
