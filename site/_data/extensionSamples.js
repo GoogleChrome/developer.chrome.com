235e864f268992d8d025b4d984f0066aef4eb1bc
daidr <daidr@daidr.me>
2023-07-31 14:10:11 +0800

    Add extension samples external data flow

---
+++ site/_data/extensionSamples.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview Extract browser extension samples from sample list data.
+ */
+
+const path = require('path');
+const fs = require('fs');
+
+module.exports = (() => {
+  const dataFile = path.join(
+    __dirname,
+    '../../external/data/extension-samples.json'
+  );
+  const extensionSamples = JSON.parse(fs.readFileSync(dataFile, 'utf-8'));
+
+  const sampleTypes = {
+    API_SAMPLE: 'API Sample',
+    FUNCTIONAL_SAMPLE: 'Functional Sample',
+  };
+
+  const availableAPIs = new Set();
+  extensionSamples.forEach(sample => {
+    sample.apis.forEach(api => {
+      availableAPIs.add(api.namespace);
+    });
+  });
+
+  return {
+    sampleTypes,
+    samples: extensionSamples.sort((a, b) =>
+      a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+    ),
+    availableAPIs: Array.from(availableAPIs.keys()).sort((a, b) =>
+      a.toLowerCase().localeCompare(b.toLowerCase())
+    ),
+  };
+})();
