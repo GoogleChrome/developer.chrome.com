e0f0464ec264de7696fec068ae21f4599da23d45
daidr <daidr@daidr.me>
2023-08-03 17:55:28 +0800

    Add external build script

--- external/build/extension-samples.js
+++ external/build/extension-samples.js
@@ -1,1 +1,63 @@
-// TODO: In this files extension-samples.json will be downloaded and saved to ../data/extension-samples.json
+/**
+ * @fileoverview Fetches chrome extension samples list from GitHub artifacts
+ * and writes to storage.
+ * https://github.com/GoogleChrome/chrome-extensions-samples
+ */
+
+const {
+  getGitHubApiClient,
+} = require('../../gulp-tasks/stageGitHub/lib/gitHubApi.js');
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+const path = require('path');
+
+const REPO_OWNER = 'GoogleChrome';
+const REPO_NAME = 'chrome-extensions-samples';
+
+async function run() {
+  const client = await getGitHubApiClient();
+
+  const response = await client.request(
+    `GET /repos/${REPO_OWNER}/${REPO_NAME}/actions/artifacts`
+  );
+
+  if (response.status !== 200) {
+    throw new Error(
+      `Could not fetch artifacts. Status code: ${response.status}`
+    );
+  }
+
+  const artifacts = response.data.artifacts.filter(
+    artifact => artifact.name === 'extension-samples.json' && !artifact.expired
+  );
+
+  if (!artifacts.length) {
+    throw new Error('No artifacts found.');
+  }
+
+  const latestArtifactId = artifacts[0].id;
+
+  const artifactResponse = await client.request(
+    `GET /repos/${REPO_OWNER}/${REPO_NAME}/actions/artifacts/${latestArtifactId}/zip`
+  );
+
+  if (artifactResponse.status !== 200) {
+    throw new Error(
+      `Could not fetch artifact data. Status code: ${artifactResponse.status}`
+    );
+  }
+
+  const zip = new AdmZip(Buffer.from(artifactResponse.data));
+
+  const zipEntries = zip
+    .getEntries()
+    .filter(entry => entry.entryName === 'extension-samples.json')
+    .map(entry => entry.getData());
+
+  fs.writeFileSync(
+    path.join(__dirname, '../data/extension-samples.json'),
+    zipEntries[0]
+  );
+}
+
+run();
