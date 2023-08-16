4480490df6e95ea6dc1ab304180c73940cb5fff6
daidr <daidr@daidr.me>
2023-08-16 19:36:58 +0800

    Add IGNORED_SAMPLES to hide specified samples

--- site/_data/extensionSamples.js
+++ site/_data/extensionSamples.js
@@ -5,13 +5,22 @@
 const path = require('path');
 const fs = require('fs');
 
+// The names of the samples matching the list will be hidden.
+const IGNORED_SAMPLES = [
+  'sample.co2meter', // WebHID is not released yet.
+];
+
 module.exports = (() => {
   const dataFile = path.join(
     __dirname,
     '../../external/data/extension-samples.json'
   );
-  /** @type {extensionSample[]} */
-  const extensionSamples = JSON.parse(fs.readFileSync(dataFile, 'utf-8'));
+
+  const extensionSamples = /** @type {extensionSample[]} */ (
+    JSON.parse(fs.readFileSync(dataFile, 'utf-8'))
+  ).filter(sample => {
+    return !IGNORED_SAMPLES.includes(sample.name);
+  });
 
   const sampleTypes = {
     API_SAMPLE: 'API Sample',
