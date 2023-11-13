7ce339336e31a83abb7e173767591af6fab163c9
Matthias Rohmer <mrohmer@google.com>
2023-11-13 20:07:48 +0100

    feat: unwrap details shortcode on export

--- site/_shortcodes/Details.js
+++ site/_shortcodes/Details.js
@@ -9,7 +9,9 @@ const md = require('../_filters/md');
  */
 function Details(content, state) {
   if (this.ctx.export) {
-    return DetailsExport.bind(this)(content, state);
+    return `{# TODO(migration): Unwrapped Details shortcode - start #}
+${content}
+{# TODO(migration): Unwrapped Details shortcode - end #}`;
   }
 
   const stateOverride = state === 'open' ? 'open' : '';
@@ -17,48 +19,6 @@ function Details(content, state) {
   return `<details class="details stack" ${stateOverride}>
 
 ${content}</details>`;
-}
-
-/**
- * @this {Object}
- * @param {*} content
- * @param {*} state
- * @returns
- */
-function DetailsExport(content, state) {
-  // Parse content to DOM, to pluck out a potential DetailsSummary/h*.showalways element
-  // and move it out of the actual content div
-  const $ = cheerio.load(`<div id="content">${content}</div>`);
-
-  const $summary = $('div.showalways');
-  let summary = '';
-  if ($summary) {
-    // The summary can have a heading element, and a preview sentence, which need
-    // to be rendered as markdown, independently
-    const $heading = $summary.find('.inline');
-    $heading.html(md.renderDevSiteInline($heading.html()));
-
-    const $preview = $summary.find('.block');
-    if ($preview.length) {
-      $preview.html(md.renderDevSiteInline($preview.html()));
-    }
-
-    summary = $summary.prop('outerHTML');
-    $('#content').html($('#content').html().replace(summary, ''));
-  }
-
-  $('#content').html(md.renderDevSite($('#content').html()));
-
-  const computedState = state === 'open' ? 'expanded' : '';
-  return `
-<div>
-  <devsite-expandable ${computedState} class="arrow-icon">
-    ${summary}
-    <div>
-      ${pretty($('#content').html())}
-    </div>
-  </devsite-expandable>
-</div>`;
 }
 
 module.exports = {Details};
